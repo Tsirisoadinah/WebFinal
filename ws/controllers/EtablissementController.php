9692522fc0a3a319de32500c2464c1e04caabbd2
Toky-Lionnel <tokyrajaonarivony@gmail.com>
2025-07-08 12:18:14 +0300

    montant total par mois

--- ws/controllers/EtablissementController.php
+++ ws/controllers/EtablissementController.php
@@ -3,20 +3,35 @@ require_once __DIR__ . '/../models/Etablissement.php';
 require_once __DIR__ . '/../helpers/Utils.php';
 
 
-class EtablissementController {
+class EtablissementController
+{
 
-    public static function getFonds () {
+    public static function getFonds()
+    {
         $fonds = Etablissement::getFondsByIdEtablissement(1);
         Flight::json($fonds);
     }
 
 
-    public static function ajouterFond() {
+    public static function ajouterFond()
+    {
         // Get the raw POST data (form-urlencoded)
         $request = Flight::request();
         $montant = (float) $request->data->montant; // Access the 'montant' field
-        
+
         Etablissement::updateFonds(1, $montant);
         Flight::json(["success" => true, "message" => "Fonds inséré avec succès"]);
+    }
+
+
+    public static function getFondsDates()
+    {
+        $dateDebut = Flight::request()->query['dateDebut'];
+        $dateFin = Flight::request()->query['dateFin'];
+
+        if (!$dateDebut || !$dateFin) {
+            Flight::halt(400, json_encode(['error' => 'Invalid date format']));
+        }
+        Flight::json(Etablissement::getSituationMensuelleParDate($dateDebut, $dateFin,1));
     }
 }
